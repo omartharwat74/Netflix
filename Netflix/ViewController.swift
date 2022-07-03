d5f9a9d0158e0448ce8a7a94dc0c8a791b23ecab
Omar Tharwat <omartharwat518@gmail.com>
2022-07-03 17:58:55 +0200

    Initial Commit

---
+++ Netflix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Netflix
+//
+//  Created by Omar Tharwat on 7/3/22.
+//  Copyright © 2022 Omar Tharwat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
